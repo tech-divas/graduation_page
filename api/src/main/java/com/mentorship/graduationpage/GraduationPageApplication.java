195365857c1544e3abb50c9168afb84b80e982a2
Alisa Kovalevskaia <kovalevskaia.av@gmail.com>
2024-02-18 16:47:58 +0200

    Added folders + Spring Boot project

---
+++ api/src/main/java/com/mentorship/graduationpage/GraduationPageApplication.java
@@ -0,0 +1,14 @@
+package com.mentorship.graduationpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class GraduationPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationPageApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/mentorship/graduationpage/GraduationPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentorship.graduationpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
