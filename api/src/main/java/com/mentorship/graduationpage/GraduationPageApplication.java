fa6ffa6fcb3e054b5dc5b86b3b9f2039c047cccd
Alisa Kovalevskaia <kovalevskaia.av@gmail.com>
2024-03-19 15:08:11 +0200

    added last dependencies

--- api/src/main/java/com/mentorship/graduationpage/GraduationPageApplication.java
+++ api/src/main/java/com/mentorship/graduationpage/GraduationPageApplication.java
@@ -2,10 +2,8 @@ package com.mentorship.graduationpage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-//		(exclude = {DataSourceAutoConfiguration.class })
 public class GraduationPageApplication {
 
 	public static void main(String[] args) {
