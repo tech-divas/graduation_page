03985ac520dd08ecb717a67bff9955348c60dcc5
Viola <pumpure@yahoo.com>
2024-04-01 23:38:06 -0600

    menu component

--- app/src/all_projects_page/AllProjectsPage.jsx
+++ app/src/all_projects_page/AllProjectsPage.jsx
@@ -1,9 +1,11 @@
 import Header from "../reusable_components/Header";
+import Menu from "../reusable_components/Menu";
 
 const AllProjectPage = () => {
   return (
     <div>
       <Header />
+      <Menu />
     </div>
   );
 };
