79bd5a14233331b24754392adeb1748124e929d1
dianaJacenko <141279078+dianaJacenko@users.noreply.github.com>
2024-04-03 12:46:19 +0300

    Footer Added
    Added footer, but footer image must be changed(better quality needed)

--- app/src/AllProjectsFolder/AllProjectsPage.jsx
+++ app/src/AllProjectsFolder/AllProjectsPage.jsx
@@ -1,10 +1,15 @@
 import Header from "../CommonCompFolder/Header";
-
+import Footer from "../CommonCompFolder/Footer";
 const AllProjectPage = () => {
   return (
+    <>
     <div>
       <Header />
     </div>
+    <div>
+      <Footer />
+    </div>
+    </>
   );
 };
 
---
+++ app/src/CommonCompFolder/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css"
+export default function Footer() {
+    return <>
+    <div className="footerContainer">
+    <p>Contact us<br /> <br />
+    mentorship@rigatechgirls.com</p>
+    </div>
+    <div className="footerImage">
+        <img src="../public/FooterImage.png" alt="Footer Photo" />
+    </div>
+    </>
+}
