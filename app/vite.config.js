214910cad88bc0840fee70cf6699cad9fa17831b
Viola <pumpure@yahoo.com>
2024-05-15 21:28:52 -0600

    workflow moved to app dir

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.MODE === "production" ? "/<graduation_page>/" : "/",
+  base: "/graduation_page/",
 });
