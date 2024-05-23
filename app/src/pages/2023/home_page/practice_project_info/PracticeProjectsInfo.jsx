5b7ce70f2f54c16cda326e341e4dc7af13f7fba1
Viola <pumpure@yahoo.com>
2024-05-22 22:54:39 -0600

    Homepage top info part

---
+++ app/src/pages/2023/home_page/practice_project_info/PracticeProjectsInfo.jsx
@@ -0,0 +1,42 @@
+import "./PracticeProjectsInfo.css";
+import birdImage from "./birdImage.jpg";
+import birdOnCircle from "./birdOnCircle.png";
+
+const PracticeProjectsInfo = () => {
+  return (
+    <div className="practiceProjectInfo">
+      <div className="titleContainer">
+        <img src={birdImage} alt="Bird" className="birdImage" />
+        <h1>Practice Projects</h1>
+      </div>
+      <div className="descriptionContainer">
+        <p>
+          Riga TechGirls mentorship program brings together tech professionals
+          eager to impart their knowledge and experience to support women in the
+          early stages of their tech careers.
+          <br />
+          <br />
+          PRACTICE PROJECTS aim to offer mentees an opportunity to showcase
+          their abilities in real-life (or near-real-life) IT projects. They can
+          apply their previously acquired skills within their chosen track,
+          experience the dynamics of an IT team, and immerse themselves in the
+          specifics of teamwork. The essence is ‘learning-by-doing’ throughout
+          the project’s life cycle.
+        </p>
+        <img
+          src={birdOnCircle}
+          alt="Bird on a circle"
+          className="birdOnCircle"
+        />
+      </div>
+      <div className="practiceProjectYears">
+        <a href="/graduation_page/projects">
+          <button>Practice Projects 2023</button>
+        </a>
+        <button>2024</button>
+      </div>
+    </div>
+  );
+};
+
+export default PracticeProjectsInfo;
