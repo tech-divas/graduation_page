5b8f9944361f00a2d37cb041bba8010e91be03d3
dianaJacenko <141279078+dianaJacenko@users.noreply.github.com>
2024-05-30 15:37:38 +0300

    Home-page-update
    Home-page-update

---
+++ app/src/pages/2023/home_page/AllParticipantDescriptions.jsx
@@ -0,0 +1,32 @@
+
+import "./AllParticipantDescriptions.css";
+import whiteBird from "./white-bird.png";
+import greenBird from "./green-bird.png";
+import yellowBird from "./yellow-bird.png";
+
+export default function AllParticipantDescriptions() {
+    return (
+        <div className="allParticipants">
+            <div className="participant mentors">
+                <img src={whiteBird} alt="white bird" className="birdImg" />
+                <div className="textContent">
+                    <p>Our program's mentors are dedicated professionals who empower women in technology through personalized guidance and support, offering 1:1 sessions and fostering open communication. They commit several hours weekly to mentoring and can also lead practice project teams, enhancing both personal and professional growth. By participating, mentors refine their leadership skills, expand their network, and make a meaningful impact in the tech industry. <button>Mentors</button></p>
+                </div>
+            </div>
+
+            <div className="participant experts-speakers">
+                <img src={greenBird} alt="green bird" className="birdImg" />
+                <div className="textContent">
+                    <p>Experts and speakers in our Mentorship program are esteemed professionals dedicated to supporting project teams and enhancing learning experiences. They guide teams by sharing valuable insights and solutions during meetings, ensuring their guidance aligns with the team's objectives. By actively engaging with project teams, they enrich participants' learning journeys and foster a collaborative environment of continuous growth and development. <button>Experts/Speakers</button></p>
+                </div>
+            </div>
+
+            <div className="participant mentees">
+                <img src={yellowBird} alt="yellow bird" className="birdImg" />
+                <div className="textContent">
+                    <p>Mentees in our program embark on a transformative six-month journey, working on practical projects and receiving personalized guidance from mentors. This supportive environment blends online and in-person interactions, offering access to local and international mentors, networking opportunities, and skill advancement. Participants engage in hands-on projects, receiving guidance from team mentors and experts, fostering a sense of community and empowerment for personal and professional growth. <button>Mentees</button></p>
+                </div>
+            </div>
+        </div>
+    );
+}
