97dab6153a69efd33af3784a181d4fe543fe9e47
dianaJacenko <141279078+dianaJacenko@users.noreply.github.com>
2024-06-09 08:37:32 +0300

    Button style
    Button style for project page

--- app/src/pages/2023/project_page/MenteesProjectComp.jsx
+++ app/src/pages/2023/project_page/MenteesProjectComp.jsx
@@ -7,7 +7,7 @@ const MenteeCard = ({ name, role, field, linkedin, onReadMore }) => {
       <h2 className="menteeRole">{role}<br></br>{name}</h2>
       <h3 className="menteeField">{field}</h3>
       <p><a href={linkedin} target="_blank" rel="noopener noreferrer" className="menteeLink">LinkedIn</a></p>
-      <button onClick={onReadMore} className="menteeButton">Read more</button>
+      <div className="projectMenteeButton"><button onClick={onReadMore} className="menteeButton">Read more</button></div>
     </div>
   );
 };
--- app/src/pages/2023/project_page/ProjectMentorComp.jsx
+++ app/src/pages/2023/project_page/ProjectMentorComp.jsx
@@ -8,7 +8,7 @@ const MentorCard = ({ name, role, field, linkedin, onReadMore }) => {
       <h2 className="mentorRole">{role}<br></br>{name}</h2>
       <h3 className="mentorField">{field}</h3>
       <p><a href={linkedin} target="_blank" rel="noopener noreferrer" className="mentorLink">LinkedIn</a></p>
-      <button onClick={onReadMore} className="mentorButton">Read more</button>
+      <div className="projectMentorButton"><button onClick={onReadMore} className="mentorButton">Read more</button></div>
     </div>
   );
 };
