ee1642bfa5062103e4594dc187566d33537e2e85
Viola <pumpure@yahoo.com>
2024-06-09 01:30:09 -0600

    Search/filter added to Mentees page

--- app/src/pages/2023/mentees_page/MenteesPage.jsx
+++ app/src/pages/2023/mentees_page/MenteesPage.jsx
@@ -1,10 +1,12 @@
 import PageTemplate from "../../../reusable_components/PageTemplate";
 import "./MenteesPage.css";
 import MenteesComp from "./MenteesComp";
+import Search from "../../../reusable_components/search/Search";
 
 const MenteesPage = () => {
   return (
     <PageTemplate>
+      <Search placeholderText="Search Mentees" />
       <MenteesComp />
     </PageTemplate>
   );
